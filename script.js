5b4951f199fc32343e75489710258f92ad8ccb27
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-04 15:01:26 +0300

    first_edition commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function darkTheme(){
+    var head = document.getElementsByTagName(head);
+    var link = document.getElementsByTagName(link);
+    //setting the attribute values to elements
+    link.rel  = "stylesheet";
+    link.href = "dark.css";
+
+    head.appen
+}
