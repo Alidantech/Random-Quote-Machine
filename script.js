d230dd9310ff38612a4e77457d83c9a0f71413cc
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-06 00:40:04 +0300

    perfection

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function darkTheme(){
-    var head = document.getElementsByTagName('head');
+    var head = document.getElementsByTagName('head')[0];
     var link = document.createElement('link');
     //setting the attribute values to elements
-    
     link.rel  = "stylesheet";
     link.type = "text/css";
     link.href = "darkMode.css";
     head.appendChild(link);
+    //document.getElementsById('dark').style.display = 'none';
 }
 function lightTheme(){
-    var head = document.getElementsByTagName("head");
-    var link = document.createElement("link ");
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement('link');
     link.rel = "stylesheet";
     link.type = "text/css";
     link.href = "lightMode.css";
     head.appendChild(link);
+   // document.getElementById('light').style.display = 'none';
 }
