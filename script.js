bb3fcb3902999c7da715f51802b8f0428053313c
PETER IRUNGU <peteralidante254@gmail.com>
2023-01-05 23:59:13 +0300

    dark mode

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function darkTheme(){
-    var head = document.getElementsByTagName(head);
-    var link = document.getElementsByTagName(link);
+    var head = document.getElementsByTagName('head');
+    var link = document.createElement('link');
     //setting the attribute values to elements
+    
     link.rel  = "stylesheet";
-    link.href = "dark.css";
+    link.type = "text/css";
+    link.href = "darkMode.css";
+    head.appendChild(link);
+}
+function lightTheme(){
+    var head = document.getElementsByTagName("head");
+    var link = document.createElement("link ");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "lightMode.css";
     head.appendChild(link);
 }
